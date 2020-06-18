9fb5deae09f89ceab22f60a536b482a4419cf582
adisak.ko <adisak.ko@chanwanich.com>
2020-06-18 17:25:34 +0700

    Updates

---
+++ precache-manifest.61912cf9337b0782c93750d5167e4e87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce1f7f6a026fa84877a01b62cebf744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "610170817dbcb667c30b",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "aa1f68fac958f37fca6a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "610170817dbcb667c30b",
+    "url": "/static/js/2.d5304168.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d5304168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa1f68fac958f37fca6a",
+    "url": "/static/js/main.f985dc30.chunk.js"
+  },
+  {
+    "revision": "6cc65c51bd7c7897046f",
+    "url": "/static/js/runtime-main.2c8f192d.js"
+  }
+]);
