7f7431e3b8272b4348bf6caf4d9156968c2f1cda
watcharapong.c <ne7shii@gmail.com>
2020-06-19 12:02:04 +0700

    update detail page

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -1,24 +1,35 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
- import {jobs} from '../data.js'
+import { jobs } from '../data.js'
 import styled from "styled-components";
+
+import { FaBookmark, FaRegCalendarAlt } from 'react-icons/fa';
+import { GrMapLocation } from 'react-icons/gr';
+import { MdLocationOn } from 'react-icons/md'
+import { GiWhiteBook } from 'react-icons/gi'
 const Styles = styled.div`
 .container-boxed {
 
   max-width: 1200px;
   margin: 0 auto;
 }
+.row {
+  width=100%;
+}
+.width33.Col {
+  width: 33%;
+}
 `;
 
- const job = jobs.find(element=>(element.jobId=='0001'))
+const job = jobs.find(element => (element.jobId == '0001'))
 export default class Detail extends Component {
 
-  componentDidMount () {
+  componentDidMount() {
     const script = document.createElement("script");
     script.src = "../data.js";
     script.async = true;
     document.body.appendChild(script);
-}
+  }
 
 
   render() {
@@ -26,13 +37,36 @@ export default class Detail extends Component {
     console.log(job)
     return (
       <Styles>
-        <Row style={{ position: 'relative', top: '154px', width: '100%', marginLeft: 0, backgroundColor: 'rgb(235,238,239)' }}>
-          <div className='container-boxed' style={{ position: 'relative', width: '100%' }}>
-    <h1>{job.position}</h1>
+        <div style={{ position: "relative", height: "154px", width: "100%" }} />
+        <Row style={{ backgroundColor: 'rgb(235,238,239)' }}>
+          <div className='container-boxed' style={{ width: '100%', color: '#444444' }}>
+            <h1>{job.position}</h1>
             <p>
-            {job.companyName} <br/>
+              {job.companyName} <br />
+              <div style={{ color: '#1380C0' }}>
+                <FaBookmark />{job.jobType}<br />
+              </div>
+              <GrMapLocation />{job.address}<br />
+              <MdLocationOn />{job.provinceTh}<br />
+              <FaRegCalendarAlt />{job.durationPost}<br />
+              <GiWhiteBook />{job.jobCategory}<br />
             </p>
           </div>
+        </Row>
+        <Row className='row container-boxed' >
+          <Col md={8}>
+            <h3>Job Overview</h3>
+            <Row className='row'>
+           
+                <Col md={4}>Employer Type</Col>
+                <Col md={4}>Employer Type</Col>
+                <Col md={4}>Employer Type</Col>
+                <Col md={4}>Employer Type</Col>
+            </Row>
+          </Col>
+          <Col md={4}>
+            <h4>{job.companyName}</h4>
+          </Col>
         </Row>
       </Styles>
     );
